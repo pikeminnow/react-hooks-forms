877ff0eb2e1c51e86c01f45647c5f55e0f8438a9
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-22 15:51:41 -0400

    lab activities

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
 
+
+
 ReactDOM.render(
-  <Form />,
+  <Form  />,
   document.getElementById('root')
 );
